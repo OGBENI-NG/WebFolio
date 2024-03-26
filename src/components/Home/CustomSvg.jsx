b9ff61b27982133d7e6076165eb1f65dce70fcd7
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-03-26 12:12:39 +0100

    testing

--- src/components/Home/CustomSvg.jsx
+++ src/components/Home/CustomSvg.jsx
@@ -14,7 +14,7 @@ const CustomSVG = ({ width, height, theme }) => {
         413.863 -30.7084 18.3093 5.79493 103.493C42.2982 
         188.676 507.904 152.389 258.307 22.4743Z`} 
         fill={`${theme === 'light' ? '#D9D9D9' : '#1A1A1A'}  `} 
-        fill-opacity={`${theme === 'light' ? '0.5' : '90'} `}
+        fill-opacity={`${theme === 'light' ? '0.5' : '100'} `}
       />
     </svg>
   );
