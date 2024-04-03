f49b5027ab8e648609de6c12d1fb221fa44f2abb
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-04-03 20:19:33 +0100

    testing

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,19 +20,23 @@ export default function NavBar({ themeColor, layoutTheme }) {
   const navBarData = navBarItems.map(item => {
     const isActive = location.pathname === (item.name.toLowerCase() === 'home' ? '/' : `/${item.name.toLowerCase()}`)
 
-    const navbarStyle = `flex flex-col gap-[3px] items-center font-OpenSan 
-      ${isActive ? 'text-brand font-extrabold' : themeColor} transition-all`
+    const navbarStyle = `flex flex-col gap-[2px] items-center font-OpenSan 
+      ${isActive 
+        ? `text-brand font-extrabold before:content-[''] before:w-[50px] 
+          before:h-[2px] before:absolute before:bg-brand before:top-0 
+          before:transition-[.5s]` 
+      : `${themeColor} before:w-[0px]` } transition-[.5s]`
     ;
 
     return (
       <NavLink
         key={item.id}
         to={item.name.toLowerCase() === 'home' ? '/' : `/${item.name.toLowerCase()}`}
-        className={navbarStyle}
+        className={`${navbarStyle}`}
         onClick={() => handleIconClick(item.id)}
       >
         <div>
-          {isActive ? item.isFocusedIcon({ size: 28 }) : item.icon({ size: 28 })}
+          {isActive ? item.isFocusedIcon({ size: 25 }) : item.icon({ size: 25 })}
         </div>
         <span className="text-[14px] font-bold">{item.name}</span>
       </NavLink>
@@ -41,7 +45,7 @@ export default function NavBar({ themeColor, layoutTheme }) {
 
   return (
     <nav className={`flex items-center justify-between
-     py-[10px] px-5 fixed bottom-0 w-full z-50 transition-[.5s]
+     pb-[10px] pt-[4px] px-5 fixed bottom-0 w-full z-50 transition-[.5s]
      ${layoutTheme } border-t-[1px] backdrop-blur-[100px] `}
     >
       {navBarData}
