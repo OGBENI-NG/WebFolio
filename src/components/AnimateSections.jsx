adf58205f4263ffeb14b37e0a6141a6d32917b15
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-04-07 12:19:11 +0100

    testing

--- src/components/AnimateSections.jsx
+++ src/components/AnimateSections.jsx
@@ -1,25 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useInView } from 'react-intersection-observer';
 
 export default function AnimateSections({ children, className }) {
-  const [isVisible, setIsVisible] = useState(false);
   const { ref, inView } = useInView({
     threshold: 0.2
   });
 
-  useEffect(() => {
-    if (inView) {
-      setIsVisible(true);
-    } else {
-      setIsVisible(false);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [inView]); // Only update when `inView` changes
+  // Only update when `inView` changes
 
   return (
     <div
       ref={ref}
-      className={`${className} opacity-0 ${isVisible ? 'animate-fadeInUp opacity-100' : 'animate-fadeOut'}`}
+      className={`${className} opacity-0 ${inView ? 'animate-fadeInUp opacity-100' : 'animate-fadeOut'}`}
     >
       {children}
     </div>
