7c765cccfe110025cdd9948232075c3c91f4f376
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-04-15 16:33:38 +0100

    testing

--- src/components/WorkItems.jsx
+++ src/components/WorkItems.jsx
@@ -21,7 +21,7 @@ export default function WorkItem({ item, layoutTheme, themeColor,
             </p>
             <div className='flex items-center ml-auto gap-4'>
               <button 
-                className='bg-brand/5 shadow-sm shadow-brand/30 text-brand p-[10px] rounded-full' 
+                className='bg-brand/5 shadow-lg shadow-brand/30 text-brand p-[10px] rounded-full' 
                 onClick={() => toggleDescription(item.id)}> {/* Pass item.id to toggleDescription */}
                 <FaChevronDown className={`transition-all  duration-500 text-lg
                   ${openId === item.id ? '-rotate-360' : 'rotate-180'}`}/>
