8110e452d95b59b6b238eca3ce267e4212be8ec6
Adeolu Miracle <101052440+OGBENI-NG@users.noreply.github.com>
2024-05-29 13:48:12 +0100

    fix-loading overflow

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -56,26 +56,28 @@ export default function Layout({ themeColor, borderColor, data, bodyPadding,
   const headerNavbarTheme = `transition-color ${borderColor} ${theme === 'light' ? 'bg-lightest' : 'bg-darkest'}`;
 
   return (
-    <div>
+    <>
       {isLoading ? (
-        <div className={`overflow-hidden transition-opacity duration-1000 
-        ${!isLoading ? 'opacity-0' : 'opacity-100'} py-[100px] lg:py-[80px] text-center`}>
-          <h1 className={`font-bold text-5xl md:text-8xl text-center mt-auto font-Solway transition-opacity duration-1000 ${themeColor} ${showWelcome ? 'opacity-100' : 'opacity-0'}`}>
-            welcome
-          </h1>
-          <p className={`px-10 pt-8 md:pt-10 md:px-24 lg:pt-6 text-xl md:text-3xl lg:text-xl
-             leading-relaxed md:leading-relaxed transition-opacity duration-1000 
-             font-semibold
-             bg-gradient-to-r from-red-600 via-brandDark to-pink-600 inline-block text-transparent bg-clip-text
-             ${showText ? 'opacity-100' : 'opacity-0'}`}>
-            I'm excited to share my projects with you.
-          </p>
-          {showLoading && (
-             <div className='grid place-content-center m-auto overflow-hidden'>
-              <LogoAnim className={`mt-[10px] pt-[140px] md:pt-[180px] lg:pt-[150px]
-              transition-all duration-1000  ${showLoading ? 'opacity-100' : 'opacity-0'}`} />
-             </div>
-          )}
+        <div className={`absolute inset-0 h-screen overflow-hidden`}>
+          <div className='flex flex-col justify-self-center items-center gap-10 my-[100px]'>
+            <div className='text-center'>
+              <h1 className={`font-bold text-5xl md:text-8xl text-center mt-auto font-Solway transition-opacity duration-1000 ${themeColor} ${showWelcome ? 'opacity-100' : 'opacity-0'}`}>
+                welcome
+              </h1>
+              <p className={`px-10 pt-8 md:pt-10 md:px-24 lg:pt-6 text-xl md:text-3xl lg:text-xl
+                leading-relaxed md:leading-relaxed transition-opacity duration-1000 
+                font-semibold
+                bg-gradient-to-r from-red-600 via-brandDark to-pink-600 inline-block text-transparent bg-clip-text
+                ${showText ? 'opacity-100' : 'opacity-0'}`}>
+                I'm excited to share my projects with you.
+              </p>
+            </div>
+            {showLoading && (
+              <div className=' my-[100px] overflow-hidden'>
+                <LogoAnim className={`transition-all duration-1000 ${showLoading ? 'opacity-100' : 'opacity-0'}`} />
+              </div>
+            )}
+          </div>
         </div>
       ) : (
         <div>
@@ -105,6 +107,6 @@ export default function Layout({ themeColor, borderColor, data, bodyPadding,
           />
         </div>
       )}
-    </div>
+    </>
   );
 }
